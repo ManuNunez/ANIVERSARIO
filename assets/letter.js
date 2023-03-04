cdce5f6802f4a9db3f31f1d9c1447991c44e67b8
MNunez <manu.nunez.2004@gmail.com>
2023-03-04 00:48:18 -0600

    .

--- assets/letter.js
+++ assets/letter.js
@@ -1,6 +1,6 @@
 const text = "TE AMO \n me encantas, gracias por estar aqui conmigo ya un año como mi novia, pareciera que fue ayer cuando nos acostabamos en el pasto "+
              "bajo el sol, pareciera que fue ayer que corria a la prepa de la emocion  ve verte de nuevo, pareciera que fue ayer cuando jugabamos minecraft "+
-             " y te veia juntar florecitas para nuestra casa, pareciera que fue ayer que te vi comer tacos al vapor, pareciera que fue ayer que te vi y me pareciste la mujer mas hermosa del mundo y aun lo eres para mi";
+             " y te veia juntar florecitas para nuestra casa, pareciera que fue ayer que te vi comer tacos al vapor, pareciera que fue ayer que te vi y me pareciste la mujer mas hermosa del mundo y aun lo eres para mi.";
 const delay = 100; // milisegundos
 
 let i = 0;
